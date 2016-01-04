15fe5345080e11cec692a11950b7ab17c5023a20
libgober <blibgober@g.harvard.edu>
2016-01-03 20:05:22 -0500

    yo

---
+++ uniform_chisquare.py
@@ -0,0 +1,126 @@
+"""
+
+Takes in an HDFStore.  Produces graphs that show rejection regions for uniform.
+
+"""
+
+import os
+import glob
+import subprocess
+import errno
+import time
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ColorConverter
+from matplotlib import collections as mc
+from helpers2 import digit_aggregate
+from scipy.stats import chisquare
+
+#SOME SETTING THAT SHOULD BE CHANGED
+all = pd.HDFStore("Real.h5")
+
+data = data[meta.ix[meta.ix[:,4].isnull(),:].index,:,:]
+meta = pd.DataFrame([i.split("_") for i in data.items])
+election_types = ["GOV","USS","USP"]
+election_years = ["2000","2004","2008","2012"]
+pvalues = [.90,.95,.99]
+print "Target Percentile " "Percent Accepted"
+
+
+for pvalue in pvalues:
+    accept_election = []
+    for el in election_types:
+        j = 1 #subplot number
+        i = 0 #x-axis location
+        fig, ((ax1, ax2, ax3, ax4)) = plt.subplots(1, 4, sharex=False, sharey=True) #each election year gets its own axis
+        for year in election_years:
+            ### SETUP AXES AND POINT STORAGE
+            ax = eval("ax" + str(j))
+            b = 0.2
+            ax.set_xlim(left=i-b)
+            points = []
+            text = []
+            #prepare to iterate over all elections in that year
+            keys = [key for key in all.keys() if year in key]
+            for key in keys:    #e.g. key = all.keys()[0]
+                sheet =  all[key] #load the sheet
+                sheet = digit_aggregate(sheet)
+                parties = [party for party in sheet.columns if 'other' not in party]
+                for party in parties:
+                    chi2, pObs = chisquare(sheet[party])
+                    accept_election.append(pObs < pvalue)
+                    points.append((i,pObs,party))
+                    if pObs >= pvalue:
+                        text.append((i,1.05,key[0:2]))
+                    i = i+1
+            np.mean(accept_election)
+            ax.plot([x[0] for x in points if "d" in x[2]],[y[1] for y in points if "d" in y[2]],"r^",color="blue") #plot dems
+            ax.plot([x[0] for x in points if "r" in x[2]],[y[1] for y in points if "r" in y[2]],"r^",color="blue") #plot dems
+
+
+
+
+
+
+        for year in election_years:
+            """ POINTS AND LINE RELATED STUFF """
+           #set x-axis to start at wherever we are on the x-axis to begin with
+
+            temp = data[meta[(meta.Year == year) & (meta.Type == el)].index]
+            for col in [0,1]:
+                for item in temp.items:
+                    p = np.percentile(temp.ix[item,1:,col],pvalue)
+                    accept_election.append(data.ix[item,0,col] < p) #record whether we'd accept or reject
+                    m = np.max(temp.ix[item,:,col])
+                    lines.append([(i,np.min(temp.ix[item,:,col])/m),(i,p/m)])
+                    #we will place a point at the observed value
+                    points.append((i,temp.ix[item,0,col]/m))
+                    if temp.ix[item,0,col] > p:
+                        text.append((i,1.05,item[0:2]))
+                    party.append(col) #add party information
+                    i = i + 1
+            lc = mc.LineCollection(lines,colors=[partycolors[foo] for foo in party])
+            ax.plot([x[0] for x in points],[y[1] for y in points],"r^")
+            ax.add_collection(lc)
+            """ ANNOTATIONS ON FIGURE """
+            for entry in text:
+                x,y,txt = entry
+                ax.text(x,y,txt,fontsize=6)
+            """ AXIS STUFF """
+            ax.set_xlim(right=i+b) #now i has changed, it is at the most right of the x-axis
+            ax.set_ylim(0,110/100.)
+            ax.yaxis.set_visible(False)
+            ax.spines['right'].set_visible(False)
+            ax.spines['left'].set_visible(False) #turn off the bar to the right
+            if j == 1:
+                ax.yaxis.set_visible(True)
+                ax.spines['left'].set_visible(True)
+            if j == 4:
+                ax.spines['right'].set_visible(True)
+            j = j+1
+            ax.tick_params(
+                axis='x',          # changes apply to the x-axis
+                which='both',      # both major and minor ticks are affected
+                bottom='off',      # ticks along the bottom edge are off
+                top='off',         # ticks along the top edge are off
+                labelbottom='off') # labels along the bottom edge are off
+            ax.tick_params(
+                axis = 'y',
+                which = 'both',
+                right = 'off'
+                )
+            ax.set_title(str(year))
+            ax.title.set_position((0.5,0))
+        fig.suptitle(el + " \nTarget Acceptance Rate: " + str(pvalue) + "; Actual " + str(100*np.mean(accept_election))[0:5],fontsize=14, fontweight='bold')
+        fig.subplots_adjust(wspace=0)
+        fig.savefig(el + str(pvalue)+"percentile_test.pdf")
+    print pvalue, np.mean(accept_election)
+
+os.chdir("..")
+
+
+
+
+
+
